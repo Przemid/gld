a242f92e76f662b1b38d7ebb5eed78395596b848
Przemysław Ida <przemyslaw.ida@gmail.com>
2019-09-16 10:34:11 +0200

    Updates

---
+++ precache-manifest.0b6267ffeb33d398541310b54c4eef68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b9be271a0a2ba2c054b1073690875e",
+    "url": "/gld/index.html"
+  },
+  {
+    "revision": "31ae732e5e787464bb4c",
+    "url": "/gld/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "28648f0031765a9cf11e",
+    "url": "/gld/static/js/2.30a8735d.chunk.js"
+  },
+  {
+    "revision": "31ae732e5e787464bb4c",
+    "url": "/gld/static/js/main.bb6843d1.chunk.js"
+  },
+  {
+    "revision": "e5323a6b14e761804184",
+    "url": "/gld/static/js/runtime~main.82d655d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gld/static/media/logo.5d5d9eef.svg"
+  }
+]);
