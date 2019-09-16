9d7f2bbfe625f51701ffdbeaeb8a971707a442bb
Przemysław Ida <przemyslaw.ida@gmail.com>
2019-09-16 16:38:02 +0200

    beta1

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,29 +1,52 @@
 import React from "react";
-import logo from "../img/logo_male.jpg";
+import Sidebar from "react-sidebar";
+import logo from "../img/logo_kolor.jpg";
 import "../css/HomePage.css";
 import ReactNoSleep from "react-no-sleep";
 import airhornFile from "../sound/airhorn.mp3";
+import settingsIcon from "../img/setting-512.png";
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: false
+    };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+
   render() {
-    let audio = new Audio(airhornFile);
+    const audio = new Audio(airhornFile);
 
     return (
       <div className="HomePage">
-        <header className="App-header">
-          <img src={logo} alt="GLD" onClick={() => audio.play()} />
-          <p>GŁDTEAM timer</p>
-          <audio id="airhorn">
-            <source src={airhornFile} type="audio/mpeg"></source>
-          </audio>
-          <ReactNoSleep>
-            {({ isOn, enable, disable }) => (
-              <button onClick={isOn ? disable : enable}>
-                {isOn ? "on" : "off"}
-              </button>
-            )}
-          </ReactNoSleep>
-        </header>
+        <Sidebar
+          sidebar={<img src={logo} className="App-logo" alt="logo" />}
+          open={this.state.sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ sidebar: { background: "white" } }}
+        >
+          <img
+            src={settingsIcon}
+            alt="Settings"
+            onClick={() => this.onSetSidebarOpen(true)}
+            style={{ width: "50px", height: "50px" }}
+          />
+
+          <header className="App-header">
+            <p className="G-logo" onClick={() => audio.play()}>
+              Powered by <b>Goliat</b>
+            </p>
+
+            <ReactNoSleep>
+              {({ isOn, enable, disable }) => (isOn = true)}
+            </ReactNoSleep>
+          </header>
+        </Sidebar>
       </div>
     );
   }
